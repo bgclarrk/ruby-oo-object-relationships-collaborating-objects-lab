a630dbba781662d57ac27fd3b356c1d6b300a247
+ <+>
2020-12-13 13:20:57 -0700

    commit tree 4f180db37a90f53b7f9be7b9ef44199433c64762

---
+++ lib/song.rb
@@ -0,0 +1,32 @@
+class Song
+    attr_accessor :name, :artist
+    @@all = []
+
+    def initialize(name)
+        @name = name
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def self.new_by_filename(file)
+        file_arr = file.split(" - ")
+        artist = file_arr[0]
+        song = file_arr[1]
+
+        song = self.new(song)
+        song.artist = artist
+
+        song.artist_name = artist
+        
+        song
+    end
+
+    def artist_name=(artist)
+        artist = Artist.find_or_create_by_name(artist)
+        artist.add_song(self)
+    end
+
+end
